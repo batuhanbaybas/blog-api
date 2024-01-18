3a4ba25c52edf150442d858187617b3b81139123
batuhanbaybas <b.baybas@gmail.com>
2024-01-18 21:20:22 +0300

    Remove unused getAllUser route

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -1,9 +1,8 @@
 import express, { Router } from "express";
-import { getAllUser, login, register } from "../controller/auth-controller";
+import { login, register } from "../controller/auth-controller";
 
 const router: Router = express.Router();
 
-router.get("/all", getAllUser);
 router.post("/register", register);
 router.post("/login", login);
 export default router;
