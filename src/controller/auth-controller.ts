20f94daa7a9a353ca9e7a5119c9d95c61f0f4196
batuhanbaybas <b.baybas@gmail.com>
2024-01-18 19:34:34 +0300

    Refactor register and login functions in auth-controller.ts

--- src/controller/auth-controller.ts
+++ src/controller/auth-controller.ts
@@ -4,46 +4,60 @@ import { prisma } from "../config/database";
 import { signJwt } from "../helper/hash";
 
 export const register = async (req: Request, res: Response) => {
-  const user = await prisma.user.create({
-    data: {
-      email: req.body.email,
-      password: req.body.password
-    }
-  });
-  const token = await signJwt(user.id);
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: req.body.email,
+        password: req.body.password
+      }
+    });
+    const token = await signJwt(user.id);
 
-  return res.status(200).json({
-    status: true,
-    message: "User created successfully",
-    token: token
-  });
+    return res.status(200).json({
+      status: true,
+      message: "User created successfully",
+      token: token
+    });
+  } catch (error: any) {
+    return res.status(400).json({
+      status: false,
+      message: error.message
+    });
+  }
 };
 
 export const login = async (req: Request, res: Response) => {
-  const user = await prisma.user.findUnique({
-    where: {
-      email: req.body.email
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: req.body.email
+      }
+    });
+    if (!user) {
+      return res.status(400).json({
+        status: false,
+        message: "User not found"
+      });
     }
-  });
-  if (!user) {
+
+    if (user?.password !== req.body.password) {
+      return res.status(400).json({
+        status: false,
+        message: "Password is incorrect"
+      });
+    }
+
+    const token = await signJwt(user?.id);
+
+    return res.status(200).json({
+      status: true,
+      message: "User logged in successfully",
+      token: token
+    });
+  } catch (error: any) {
     return res.status(400).json({
       status: false,
-      message: "User not found"
+      message: error.message
     });
   }
-
-  if (user?.password !== req.body.password) {
-    return res.status(400).json({
-      status: false,
-      message: "Password is incorrect"
-    });
-  }
-
-  const token = await signJwt(user?.id);
-
-  return res.status(200).json({
-    status: true,
-    message: "User logged in successfully",
-    token: token
-  });
 };
